df25a3196d8237a6c566a3f4887c719cfb511f8c
Marc Lambrichs <mlambrichs@melange-it.nl>
2018-10-23 07:18:11 +0200

    Add missing etcd flag in database profile

--- manifests/database.pp
+++ manifests/database.pp
@@ -3,18 +3,23 @@
 # @example when declaring the metrics class
 #  class { '::profiles::database::metrics': }
 #
+# @param etcd       Manage etcd on this node.
 # @param influxdb   Manage influxdb on this node.
 # @param mongodb    Manage mongodb on this node.
 # @param mysql      Manage mysql on this node.
 # @param neo4j      Manage neo4j on this node.
 # @param postgresql Manage postgresql on this node.
 class profiles::database (
+  Boolean $etcd = false,
   Boolean $influxdb = false,
   Boolean $mongodb = false,
   Boolean $mysql = false,
   Boolean $neo4j = false,
   Boolean $postgresql = false,
 ) {
+  if $etcd {
+    class { '::profiles::database::etcd':}
+  }
   if $influxdb {
     class { '::profiles::database::influxdb':}
   }
