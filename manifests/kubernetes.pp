89eb5bd42f824e3ec4827fa247a0cfaa0571a453
Marc Lambrichs <mlambrichs@melange-it.nl>
2018-10-24 17:55:55 +0200

    Use puppetlabs kubernetes module

--- manifests/kubernetes.pp
+++ manifests/kubernetes.pp
@@ -3,15 +3,10 @@
 # @example when declaring the kubernetes class
 #  class { '::profiles::kubernetes': }
 #
-# @param flannel    Manage flannel on this node.
-# @param kubernetes Manage kubernetes on this node.
+# @param kubernetes Manage kubernetes on this node
 class profiles::kubernetes (
-  Boolean $flannel = false,
   Boolean $kubernetes = false,
 ) {
-  if $flannel {
-    class { '::profiles::kubernetes::flannel': }
-  }
   if $kubernetes {
     class { '::profiles::kubernetes::kubernetes': }
   }
--- manifests/kubernetes/kubernetes.pp
+++ manifests/kubernetes/kubernetes.pp
@@ -3,25 +3,20 @@
 # @example when declaring the kubernetes class
 #  class { '::profiles::kubernetes::kubernetes': }
 #
-class profiles::kubernetes::kubernetes {
-    include ::kubernetes::master
-    include ::kubernetes::client
-
-    class { 'kubernetes::master::apiserver':
-      advertise_address        => '0.0.0.0',
-      insecure_bind_address    => '0.0.0.0',
-      bind_address             => '0.0.0.0',
-      service_cluster_ip_range => '10.254.1.0/16',
-      admission_control        => [
-        'NamespaceLifecycle',
-        'NamespaceExists',
-        'LimitRanger',
-        'SecurityContextDeny',
-        'ResourceQuota',
-      ],
+# @param controller 
+# @param worker
+class profiles::kubernetes::kubernetes (
+  Boolean $controller = false,
+  Boolean $worker = false,
+) {
+    if $controller {
+      class { 'kubernetes':
+        controller => true,
+      }
     }
-    class { 'kubernetes::master::controller_manager': }
-    class { 'kubernetes::master::scheduler': }
-    class { 'kubernetes::node::kubelet': }
-    class { 'kubernetes::node::kube_proxy': }
+    if $worker {
+      class { 'kubernetes':
+        worker => true,
+      }
+    }
 }
