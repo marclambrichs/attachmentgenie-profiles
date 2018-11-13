f962660148f926ac4a15b6bfe7284c141c01c8a1
Marc Lambrichs <mlambrichs@melange-it.nl>
2018-11-13 04:15:48 +0100

    Add param restart_on_change

--- manifests/monitoring/logstash.pp
+++ manifests/monitoring/logstash.pp
@@ -3,14 +3,15 @@
 # @example when declaring the logstash class
 #  class { '::profiles::monitoring::logstash': }
 #
-# @param auto_upgrade Install the latest version.
-# @param config_files Content for logstash input, filters and output.
-# @param ensure       Present or absent.
-# @param group        Logstash group.
-# @param manage_repo  Setup repository to install logstash from.
-# @param pipelines    Define array of pipelines.
-# @param user         Logstash user.
-# @param version      Which version of logstash to install.
+# @param auto_upgrade      Install the latest version.
+# @param config_files      Content for logstash input, filters and output.
+# @param ensure            Present or absent.
+# @param group             Logstash group.
+# @param manage_repo       Setup repository to install logstash from.
+# @param pipelines         Define array of pipelines.
+# @param restart_on_change Restart the service whenever the configuration changes.
+# @param user              Logstash user.
+# @param version           Which version of logstash to install.
 class profiles::monitoring::logstash (
   Boolean $auto_upgrade = false,
   Hash $config_files = {},
@@ -19,18 +20,21 @@ class profiles::monitoring::logstash (
   Array $jvm_options = [],
   Boolean $manage_repo = false,
   Array $pipelines = [],
+  Boolean $restart_on_change = false,
   String $user = 'logstash',
   String $version = '6.4'
 ){
   class { '::logstash':
-    ensure         => $ensure,
-    auto_upgrade   => $auto_upgrade,
-    jvm_options    => $jvm_options,
-    logstash_group => $group,
-    logstash_user  => $user,
-    manage_repo    => $manage_repo,
-    pipelines      => $pipelines,
-    version        => $version,
+    ensure            => $ensure,
+    auto_upgrade      => $auto_upgrade,
+    jvm_options       => $jvm_options,
+    logstash_group    => $group,
+    logstash_user     => $user,
+    manage_repo       => $manage_repo,
+    pipelines         => $pipelines,
+    restart_on_change => $restart_on_change,
+    version           => $version,
   }
+
   create_resources(::logstash::configfile, $config_files)
 }
