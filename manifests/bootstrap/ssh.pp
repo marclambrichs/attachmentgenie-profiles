c24e1c08999cd51cf3952f514bf43279f5d623d4
Marc Lambrichs <mlambrichs@melange-it.nl>
2018-12-12 05:50:56 +0100

    Adapt params for ghoneycutt's puppet-module-ssh

--- manifests/bootstrap/ssh.pp
+++ manifests/bootstrap/ssh.pp
@@ -3,27 +3,29 @@
 # @example when declaring the ssh class
 #  class { '::profiles::bootstrap::ssh': }
 #
-# @param allow_agent_forwarding   Allow forwarding of the agent.
-# @param forward_agent            Forward the connection of the agent to the remote machine.
-# @param password_authentication  Accept access using password.
-# @param port                     Port to bind to.
+# @param config_entries                   Hash of config entries for a specific user's ~/.ssh/config.
+# @param permit_root_login                Allow root login.
+# @param ssh_config_forward_agent         Forward the connection of the agent to the remote machine.
+# @param sshd_config_allowagentforwarding Allow forwarding of the agent.
+# @param sshd_config_match                Hash for matches with nested arrays for options for the Match directive for the SSH daemon.
+# @param sshd_config_port                 Port to bind to.
+# @param sshd_password_authentication     Accept access using password.
 class profiles::bootstrap::ssh (
-  String $allow_agent_forwarding = 'no',
-  String $forward_agent = 'no',
-  String $password_authentication = 'no',
-  String $port = '22',
+  Hash $config_entries = {},
+  String $permit_root_login = 'no',
+  String $ssh_config_forward_agent = 'no',
+  String $sshd_config_allowagentforwarding = 'no',
+  Hash $sshd_config_match = {},
+  String $sshd_config_port = '22',
+  String $sshd_password_authentication = 'no'
 ) {
-  class { '::ssh::client':
-    forward_agent           => $forward_agent,
-    password_authentication => $password_authentication,
-  }
-  class { '::ssh::server':
-    allow_agent_forwarding  => $allow_agent_forwarding,
-    password_authentication => $password_authentication,
-    port                    => $port,
-  }
-
-  profiles::bootstrap::firewall::entry { '010 allow ssh':
-    port => Integer($port),
+  class { '::ssh':
+    config_entries                   => $config_entries,
+    permit_root_login                => $permit_root_login,
+    ssh_config_forward_agent         => $ssh_config_forward_agent,
+    sshd_config_allowagentforwarding => $sshd_config_allowagentforwarding,
+    sshd_config_match                => $sshd_config_match,
+    sshd_config_port                 => $sshd_config_port,
+    sshd_password_authentication     => $sshd_password_authentication,
   }
 }
