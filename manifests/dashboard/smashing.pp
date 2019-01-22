f3ae4532b508542ebdb3bca60a4a9fa8d8309ea7
Marc Lambrichs <mlambrichs@melange-it.nl>
2019-01-22 15:56:09 +0100

    Use rvm in smashing profile
    - Add gnupg
    - Add public gpg key
    - Add rvm

--- manifests/dashboard/smashing.pp
+++ manifests/dashboard/smashing.pp
@@ -1,12 +1,37 @@
-# This class can be used install user smashing properties
+# This class can be used install a smashing dashboard
 #
 # @example when declaring the smashing class
 #  class { '::profiles::dashboard::smashing': }
 #
 # @param provider   Package provider
+# @param system_rubies
+# @param system_users
+# @param rvm_gems
 class profiles::dashboard::smashing (
-  String $provider = 'gem'
+  String $provider = 'gem',
+  Hash $system_rubies = {},
+  Array $system_users = [],
+  Hash $rvm_gems = {}, 
 ){
+
+  if $provider == 'gem' {
+    include '::gnupg'
+  
+    gnupg_key { 'pool.sks-keyservers.net':
+      ensure     => present,
+      key_id     => '39499BDB',
+      user       => 'root',
+      key_server => 'hkp://pool.sks-keyservers.net',
+      key_type   => 'public',
+    }
+  
+    class { '::rvm': 
+      system_rubies => $system_rubies,
+      system_users  => $system_users,
+      rvm_gems      => $rvm_gems,
+    }
+  }
+
   package { 'smashing' :
     ensure   => installed,
     provider => $provider,
