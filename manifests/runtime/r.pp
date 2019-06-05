fe2ca7383d0ff83faec62845297b14cc41ce0fc0
Marc Lambrichs <mlambrichs@melange-it.nl>
2019-06-05 09:33:22 +0200

    Add feature flag for R development package

--- manifests/runtime/r.pp
+++ manifests/runtime/r.pp
@@ -5,9 +5,16 @@
 #
 # @param packages List of packages to install
 class profiles::runtime::r (
+  Boolean $devel   = false,
   Hash $packages   = {},
+  Hash $r_packages = {}
 ) {
-  class { '::r': }
+  class { '::r':
+    devel => $devel,
+  }
 
-#  create_resources('r::package', $packages)
+  Package<| tag == 'do_a' |> -> R::Package<| tag == 'do_b' |>
+
+  create_resources('package', $packages, { tag => 'do_a' })
+  create_resources('r::package', $r_packages, { tag => 'do_b' })
 }
